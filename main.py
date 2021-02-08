0dc3889f1be7baf68e2cf58625ee4e5faa74a1e5
Mario A. Lugo <mario.luggar@gmail.com>
2021-02-08 15:10:27 -0600

    feat(OpenGL) Add write text function
    Also clean the code

--- main.py
+++ main.py
@@ -2,54 +2,32 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from figure import *
 
-##Vertices 0-7  (x,y,z)
-vertices= ( 
-    (3, -.4, -1.5),
-    (3, .4, -1.5),
-    (-3, .4, -1.5),
-    (-3, -.4, -1.5),
-    (3, -.4, 1.5),
-    (3, .4, 1.5),
-    (-3, .4, 1.5),
-    (-3, -.4, 1.5)
-    )
+def InitPygame():
+    global display
+    pygame.init()
+    display = (640,480)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    pygame.display.set_caption('IMU visualizer')
 
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,6),
-    (5,1),
-    (5,4),
-    (5,6),
-    (7,3),
-    (7,4),
-    (7,6)
-)
 
-surfaces = (   
-    (0,1,2,3),
-    (4,5,6,7),
-    (1,5,4,0),
-    (3,2,6,7),
-    (1,2,6,5),
-    (0,3,7,4)
-)
+def InitGL():
+    glClearColor((1.0/255*46),(1.0/255*45),(1.0/255*64),1)
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
 
-colors = (
-    ((1.0/255*41),(1.0/255*217),(1.0/255*152)),
-    ((1.0/255*41),(1.0/255*217),(1.0/255*152)),
-    ((1.0/255*242),(1.0/255*66),(1.0/255*128)),
-    ((1.0/255*242),(1.0/255*66),(1.0/255*128)),
-    ((1.0/255*19),(1.0/255*94),(1.0/255*242)),
-    ((1.0/255*19),(1.0/255*94),(1.0/255*242)),      
-)
+    gluPerspective(100, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0, -5)
 
-def DrawBoard():
-    
+def DrawText(textString):     
+    font = pygame.font.SysFont ("Courier New",25, True)
+    textSurface = font.render(textString, True, (255,255,0), (46,45,64,255))     
+    textData = pygame.image.tostring(textSurface, "RGBA", True)         
+    glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)    
+
+def DrawBoard(): 
     glBegin(GL_QUADS)
     x = 0
     for surface in surfaces:
@@ -61,19 +39,9 @@ def DrawBoard():
     glEnd()
     
 def main():
-    pygame.init()
-    display = (640,480)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-
-    glClearColor((1.0/255*46),(1.0/255*45),(1.0/255*64),1)
-    glEnable(GL_DEPTH_TEST)
-    glDepthFunc(GL_LEQUAL)
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
-
-    gluPerspective(100, (display[0]/display[1]), 0.1, 50.0)
-
-    glTranslatef(0.0,0.0, -5)
-
+   
+    InitPygame()
+    InitGL()
 
     while True:
         for event in pygame.event.get():
@@ -100,6 +68,7 @@ def main():
         glRotatef(1, 3, 1, 1)
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        DrawText("My text")
         DrawBoard()
         pygame.display.flip()
         pygame.time.wait(10)
